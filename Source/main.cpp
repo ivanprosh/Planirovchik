90ca53390b5168205e301d56dcb6d3210f4f6ee3
ivanprosh <ivanprosh@gmail.com>
2016-07-11 21:15:41 +0300

    Доработан алгоритм (но не проверен на разных исходных данных)

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,4 +1,3 @@
-
 #include "stdafx.h"
 #include <conio.h>
 #include <stdlib.h>
@@ -45,12 +44,11 @@ class TDetail
 {
 public:
     int id; //»дентификатор детали
-    bool state; //текущее состо€ние детали (free - 0, busy - 1, finish - 2)
+    int state; //текущее состо€ние детали (free - 0, busy - 1, finish - 2)
     int curtime;//текущее врем€ дл€ обработки
     DetailType Type;
     bool in_rab;
     int cur_mach; //текущий индекс станка дл€ обработки
-    //int ctime, cstate;
     TDetail(DetailType curType):Type(curType),curtime(curType.WorkTime),in_rab(0),cur_mach(0),state(0){}
 };
 
@@ -71,9 +69,7 @@ int MaxT = 5;
 int tick = 0;
 int count = 0;
 
-//int descr_c;
-
-//int rab_det_cnt = 0;
+//функтор используетс€ дл€ алгоритма сортировки контейнеров
 struct cmp_detail {
 
    explicit cmp_detail(const TDetail*curGoal) : goal(curGoal) {}
@@ -94,76 +90,65 @@ bool process_det(TDetail* curDet, TMachine* curMachine, int time)
     if(curDet->curtime>0) return 0;
     vector<TDetail*>::const_iterator it;
     it=find_if(curMachine->Details.begin(),curMachine->Details.end(),cmp_detail(curDet));
-    int mach = curDet->cur_mach;
     if(curDet->id==24){
         int j=0;
     }
     if(it!=curMachine->Details.end()) curMachine->Details.erase(it);
     curMachine->readyfuture = 1;
-    mach = curDet->cur_mach;
     return 1;
+}
+void work(int i, int time)
+{
+    int index_machine = vecDetails[i]->Type.stanok_i[vecDetails[i]->cur_mach];
+
+    if(vecMachines.at(index_machine)->ready && (vecDetails[i]->state==0)){
+        vecMachines.at(index_machine)->Details.push_back(vecDetails[i]);
+        vecDetails[i]->state = 1;
+        if(vecMachines.at(index_machine)->Details.size()==vecMachines.at(index_machine)->rab_n)
+           vecMachines.at(index_machine)->ready = false;
+    }
+
+    if(vecDetails[i]->state == 1){
+        if(process_det(vecDetails[i],vecMachines.at(index_machine),time))
+        {
+            if(vecDetails[i]->cur_mach<(vecDetails[i]->Type.MachCount-1))
+            {
+                vecDetails[i]->cur_mach++;
+                vecDetails[i]->curtime = vecDetails[i]->Type.WorkTime;
+                vecDetails[i]->state = 0;
+            }
+            else
+            {
+                ::count--;
+                vecDetails[i]->cur_mach = 0;
+                vecDetails[i]->curtime = vecDetails[i]->Type.WorkTime;
+                vecDetails[i]->state = 2;
+            }
+        }
+    }
 }
 
 void lcfs(int i)
 {
-    //cout << "Thread is " << i << endl;
-
-    while(vecDetails[i]->state != 2)//пока не обработана
+    while(::count>0)//пока есть детали
     {
         // ∆дем начала кванта
         WaitForSingleObject(semaphore[i], INFINITE);
-        //cout << "Kvant " << tick << "Thread " << i << endl;
 
-        sortVecDetails.pop_back();
+        if(!sortVecDetails.empty()) sortVecDetails.pop_back();
+        // ќсновной алгоритм обработки детали
+        work(i,MaxT);
 
-        int index_machine = vecDetails[i]->Type.stanok_i[vecDetails[i]->cur_mach];
-        if(i==24){
-            int id = vecDetails[i]->id;
-            int state = vecDetails[i]->state;
-            bool ready = vecMachines.at(index_machine)->ready;
-            int j=0;
+        if(vecDetails[i]->state != 2) std::this_thread::sleep_for(std::chrono::milliseconds(MaxT));
+        //освобождаем семафор синхронизации по окончании работы потока
+        ReleaseSemaphore(semaphore_sync,vecThreads.size(),NULL);
+
+        if(vecDetails[i]->state != 2) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(0));//отдадим текущий квант другим потокам
+            sortVecDetails.push_back(vecDetails[i]); //вернем поток в очередь
         }
-        if(vecMachines.at(index_machine)->ready && !vecDetails[i]->state){
-            vecMachines.at(index_machine)->Details.push_back(vecDetails[i]);
-            vecDetails[i]->state = 1;
-            if(vecMachines.at(index_machine)->Details.size()==vecMachines.at(index_machine)->rab_n)
-               vecMachines.at(index_machine)->ready = false;
-        }
-
-        if(vecDetails[i]->state == 1){
-            if(process_det(vecDetails[i],vecMachines.at(index_machine),MaxT))
-            {
-                char stanok = Ch(vecDetails[i]->Type.stanok_i[vecDetails[i]->cur_mach]);
-                if(vecDetails[i]->cur_mach<(vecDetails[i]->Type.MachCount-1))
-                {
-                    int MachCount = vecDetails[i]->Type.MachCount;
-                    int cur_mach = vecDetails[i]->cur_mach;
-
-                    vecDetails[i]->cur_mach++;
-                    vecDetails[i]->curtime = vecDetails[i]->Type.WorkTime;
-                    vecDetails[i]->state = 0;
-                }
-                else
-                {
-                    int MachCount = vecDetails[i]->Type.MachCount;
-                    int cur_mach = vecDetails[i]->cur_mach;
-                    if(i==24)
-                        int j=0;
-                    int curcount = ::count--;
-                    vecDetails[i]->cur_mach = 0;
-                    vecDetails[i]->curtime = vecDetails[i]->Type.WorkTime;
-                    vecDetails[i]->state = 2;
-                }
-                stanok = Ch(vecDetails[i]->Type.stanok_i[vecDetails[i]->cur_mach]);
-            }
-        }
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(MaxT));
-        //освобождаем бит дл€ синхронизации по окончании кванта времени
-        ReleaseSemaphore(semaphore_sync,1,NULL);
-        //освобождаем следующий поток на исполнение
-        if(!sortVecDetails.empty()) ReleaseSemaphore(semaphore[sortVecDetails[sortVecDetails.size()-1]->id], 1, NULL);
     }
+
 }
 // ƒл€ сортировки по возрастанию приоритета
 bool cmp_det_pri( TDetail *d1, TDetail *d2 )
@@ -171,11 +156,29 @@ bool cmp_det_pri( TDetail *d1, TDetail *d2 )
   return d1->Type.pri < d2->Type.pri;
 }
 
+//алгоритм Round Robin
 void rr(int i)
 {
-    ;
-}
+    while(::count>0)//пока есть детали
+    {
+        // ∆дем начала кванта
+        WaitForSingleObject(semaphore[i], INFINITE);
+        // »звлекаем текущий поток из карусели на данный цикл
+        if(!sortVecDetails.empty()) sortVecDetails.pop_back();
+        // ќсновной алгоритм обработки детали
+        work(i,cpu_time);
 
+        // ≈сли поток совершил работу, то усыпл€ем его на врем€ кванта
+        if(vecDetails[i]->state != 2) std::this_thread::sleep_for(std::chrono::milliseconds(cpu_time));
+
+        //освобождаем бит дл€ синхронизации по окончании кванта времени
+        ReleaseSemaphore(semaphore_sync,1,NULL);
+
+        //освобождаем следующий поток на исполнение
+        if(!sortVecDetails.empty()) ReleaseSemaphore(semaphore[sortVecDetails[sortVecDetails.size()-1]->id], 1, NULL);
+    }
+}
+//„тение из файла
 bool rfile(const string& name)
 {
     ifstream file;
@@ -203,7 +206,7 @@ bool rfile(const string& name)
             return 0;
         }
 
-        file >> cpu_time >> MaxT >> MaxP;//задержка, врем€ обработки, макс.приоритет
+        file >> cpu_time >> MaxT >> MaxP;//квант, макс.врем€ CPU_burst, макс.приоритет
         //cout << "cpu_time " << cpu_time << "MaxT" << MaxT <<"MaxP "<< MaxP <<endl;
         // —писок машин
         for( i = 0; i < MachinesCount; i++ )
@@ -265,14 +268,40 @@ bool rfile(const string& name)
         return 0;
     }
 }
+//¬ывод результатов на экран
+void print()
+{
+
+    cout << setw( 4 );
+    cout << tick++ << "- ";
+
+    for(int i = 0; i < vecDetails.size(); i++ )
+    {
+        if (vecDetails.at(i)->state==0)
+            cout << "   w";
+        else if (vecDetails.at(i)->state==2)
+            printf( "    " );
+        else
+        {
+            int cur_mach = vecDetails.at(i)->cur_mach;
+            cout << setw( 3 );
+            cout << vecDetails.at(i)->curtime << Ch(vecDetails.at(i)->Type.stanok_i[cur_mach]); // вывод станка и оставшегос€ времени
+        }
+    }
+
+    cout << " # " ;
+    for(int i = 0; i < vecMachines.size(); i++ )
+        cout << " " << vecMachines[i]->Details.size();
+
+    cout << " # " << ::count << endl;
+
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
   if(!rfile("g:\\WORK\\_QT\\_FREELANCE\\Planirovanie\\Lab1_14\\input.txt")) return 0;
 
-  semaphore_sync = CreateSemaphore( NULL, 0, 4, NULL );
-  //priority_queue<TDetail> query;
-
+  //—оздаем потоки
   for(int i=0;i<vecDetails.size();i++)
   {
     semaphore[i] = CreateSemaphore( NULL, 0, 1, NULL );
@@ -280,56 +309,33 @@ int _tmain(int argc, _TCHAR* argv[])
     vecThreads.push_back(thr);
   }
 
-  while (true)
-  {
+  //—емафор дл€ синхронизации
+  semaphore_sync = CreateSemaphore( NULL, 0, vecThreads.size(), NULL );
+  sortVecDetails = vecDetails; //вектор дл€ очереди
 
-      sortVecDetails = vecDetails;
+  while (::count>0)
+  {
+      //дл€ RR сортируем по приоритету каждый новый цикл карусели
+      if(algorithm == rr) {
+          sortVecDetails = vecDetails;
+      }
       std::sort( sortVecDetails.begin(), sortVecDetails.end(), cmp_det_pri );
 
       //провер€ем готовность станков
       for(int i=0;i<vecMachines.size();i++)
       {
-          vecMachines.at(i)->ready = vecMachines.at(i)->readyfuture;
+          vecMachines.at(i)->ready |= vecMachines.at(i)->readyfuture;
           vecMachines.at(i)->readyfuture = 0;
       }
-
-//      for(int i=0;i<sortVecDetails.size();i++)
-//      {
-//        cout << "Current pri queue "<< sortVecDetails[i]->Type.pri << "Current id in queue "<< sortVecDetails[i]->id << endl;
-//      }
-
+      //освобождаем семафор дл€ самого приоритетного потока
       ReleaseSemaphore(semaphore[sortVecDetails[sortVecDetails.size()-1]->id], 1, NULL);
-
+      //ждем пока вс€ карусель не пройдет круг (RR) или пока поток не завершитс€(LCFS)
       for (int i = 0; i < vecThreads.size(); i++) WaitForSingleObject(semaphore_sync, INFINITE );
 
-
-      cout << setw( 4 );
-      cout << tick++ << "- ";
-
-      for(int i = 0; i < vecDetails.size(); i++ )
-      {
-          if (vecDetails.at(i)->state==0)
-              cout << "   w";
-          else if (vecDetails.at(i)->state==2)
-              printf( "    " );
-          else
-          {
-              int cur_mach = vecDetails.at(i)->cur_mach;
-              cout << setw( 3 );
-              cout << vecDetails.at(i)->curtime << Ch(vecDetails.at(i)->Type.stanok_i[cur_mach]); // вывод станка и оставшегос€ времени
-          }
-      }
-
-      cout << " # " ;
-      for(int i = 0; i < vecMachines.size(); i++ )
-          cout << " " << vecMachines[i]->Details.size();
-
-      cout << " # " << ::count << endl;
-      if(::count<=0) break;
+      //вывод на экран
+      print();
 
   }
-
-
 }
 
 
